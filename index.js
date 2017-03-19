32a91230bd35f21e52d95e5f79ecb689dc8df0fa
Lilletass <terje.loenoey@norwegian.com>
2017-03-19 18:58:46 +0100

    Added Question for my home address

--- index.js
+++ index.js
@@ -15,10 +15,21 @@ var handlers = {
       this.emit(':ask', address + ' will be set as your home address, is that correct?', 'say yes to verify or no to cancel')
     },
 
+    'AskHomeAddress': function () {
+      var address = this.attributes["homeAddress"]
+
+      if (!address) {
+        this.emit(':tell', 'No home address is set')
+      } else {
+        this.emit(':tell', address + ' is set as your home address')
+      }
+    },
+
     'AMAZON.YesIntent': function() {
         if (state == states.SETTING_ADDRESS_STATE) {
-          this.emit(':tell', askParams.address + ' is now set as your home address')
           state = ""
+          this.attributes["homeAddress"] = askParams.address
+          this.emit(':tell', askParams.address + ' is now set as your home address')
         }
     },
 
@@ -29,13 +40,21 @@ var handlers = {
         }
     },
 
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.emit(':saveState', true);
+    },
+
     'Unhandled': function() {
-        this.emit(':tell', 'Set a home address plix');
+        var message = "Welcome to NorPort, you can ask me for any public transport departures, but first you must set a home address."
+        this.emit(':tell', message);
     }
 };
 
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context)
+    alexa.appId = 'amzn1.ask.skill.3458de80-f334-42e6-806c-833b0241ef92'
+    alexa.dynamoDBTableName = 'NorPort'
     alexa.registerHandlers(handlers)
     alexa.execute()
 };

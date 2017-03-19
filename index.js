018867f17cc60c9e69bbe46f7e2357be62e62ada
Lilletass <terje.loenoey@norwegian.com>
2017-03-19 17:23:29 +0100

    Added state handlers

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 var Alexa = require('alexa-sdk');
 
+const states = {
+  SETTING_ADDRESS_STATE: "_SETTING_ADDRESS"
+}
+
+var state = ""
+var askParams = {}
+
 var handlers = {
     'SetHomeAddress': function () {
-      var address = this.event.request.intent.slots.Address.value;
-      this.emit(':ask', 'Do you want to set ' + address + ' as your home address?')
+      var address = this.event.request.intent.slots.Address.value
+      state = states.SETTING_ADDRESS_STATE
+      askParams.address = address
+      this.emit(':ask', address + ' will be set as your home address', 'say yes to verify or no to cancel')
     },
 
     'GetNextDeparture': function () {
-        this.emit(':tell', 'Hello World!');
+        this.emit(':tell', 'Hello World!')
+    },
+
+    'AMAZON.YesIntent': function() {
+        if (this.state == states.SETTING_ADDRESS_STATE) {
+          this.emit(':tell', askParams.address + ' is now set as your home address')
+        }
+    },
+
+    'AMAZON.NoIntent': function () {
+        if (this.state == states.SETTING_ADDRESS_STATE) {
+          this.emit(':tell', 'A home address needs to be set in order to know where you are travelling from')
+        }
+    },
+
+    'Unhandled': function() {
+        this.emit(':tell', 'Set a home address plix');
     }
 };
 
 exports.handler = function(event, context, callback){
-    var alexa = Alexa.handler(event, context);
+    var alexa = Alexa.handler(event, context)
     alexa.registerHandlers(handlers)
     alexa.execute()
 };
